9cb6d0770b378eabf7f9f65d910085f0027a9af2
Samuel <samuelfs96@gmail.com>
2022-09-25 19:18:19 -0400

    pokedex colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pk-red': '#bd0f34',
+        'pk-yellow': '#e2e615',
+        'pk-blue': '#31a7f1',
+        'pk-black': '#032d29',
+        'pk-light-gray': '#dedede',
+        'pk-dark-gray': '#6a6a72',
+        'pk-green': '#55ab62',
+        'pk-light-yellow': '#f1f2d3',
+        'pk-dark-blue': '#095378',
+        'pk-dark-red': '#9c0a26'
+      },
+    },
   },
   plugins: [],
 }
